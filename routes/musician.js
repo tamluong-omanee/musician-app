c8e054ca8d40fd1417ab9c6030b60cb163ea39ab
Tam Luong <ta.luong@ext.tricentis.com>
2022-04-13 11:59:19 +0700

    changed

--- routes/musician.js
+++ routes/musician.js
@@ -6,11 +6,11 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("apps ok!");
 });
 
 // retrieve all musicians from data store
-router.get('/all', (req, res) => {
+rou ter.get('/all', (req, res) => {
   const { musician } = req.app.locals;
   musician.getMusicians(req.params.id, (err, returnedMusicians) => {
     if (err) {
